e0464a994195eb070d594fe593386b447b4f84e4
aprila13.name <aprilacraig1@gmail.com>
2021-04-22 15:46:46 -0400

    4/22

---
+++ Java Group Practice/src/com/tts/Devices.java
@@ -0,0 +1,51 @@
+package com.tts;
+
+//com.tts.Devices is the class
+public class Devices {
+        String name; //These are instance variables
+        String company;
+        String type;
+        int cost;
+
+     //Constructor
+    public Devices(String name, String company, String type, int cost) {
+        this.name = name; //this.name is instance variable = name is a parameter, Data binding
+        this.company = company;
+        this.type = type;
+        this.cost = cost;
+    }
+
+
+    //Getters + Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
---
+++ Java Group Practice/src/com/tts/Main.java
@@ -0,0 +1,78 @@
+package com.tts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Loops
+        //While Loop - Condition checked at start of the loop -- if condition is false when starting, body isn't run
+        //while (condition) {body; iterator}
+//        int birthYear = 1992;
+//        int currentYear = 2021;
+//
+//        while (currentYear >= birthYear) {
+//            System.out.println(currentYear);
+//            currentYear--;
+//        }
+
+        //Do-While Loop - Condition is checked at end of the loop -- Always runs at least once
+        //do {body} while (condition)
+//        int myNum = 10;
+//
+//        do {
+//            System.out.println(myNum);
+//        } while (myNum < 10); // 10 isn't less than 10 but prints anyway
+
+
+        //For Loop - Condition checked at the start of the loop
+        //for (initialize; condition; iterator) {body}
+//        for(int i = 50; i <= 70; i+=3) {
+//            System.out.println("The number is " + i);
+//        }
+
+//        for(int i = 10; i >= 0; i--) {
+//            System.out.println("The number is " + i);
+//        }
+
+//        //Arrays are fixed, ArrayList are dynamic w/o predetermined size
+//        String[] myGroup = {"April", "Eyke", "Echu", "Laura", "Jonathan"}; //Array
+//
+//        System.out.println(myGroup[0]); //Must use an index to print an array
+
+        //ForEach Loop - runs a statement once for each element
+//        for (DataType loopVariable : Array) {body}
+
+//        for(String people : myGroup) {
+//            System.out.println(people);
+//        }
+
+//        List<String> myGroup = new ArrayList<>();
+//        myGroup.add("April");
+//        myGroup.add( "Jonathan");
+//        myGroup.add("Eyke");
+//        myGroup.add("Laura");
+//        myGroup.add("Echu");
+////
+//        System.out.println(myGroup); //Can print an entire ArrayList
+////
+//        for(String people : myGroup) {
+//            System.out.println(people);
+//        }
+
+//        Devices iPhone = new Devices("iPhone", "Apple", "Cell Phone", 800);
+//        System.out.println(iPhone);//won't print entire object
+//        System.out.println(iPhone.name); //iPhone
+//
+//        iPhone.setName("Samsung"); //Using getters and setters
+//        System.out.println(iPhone.getName());
+    }
+}
+
+
+
+
+
+
+
